cc4c891cece88172d3c6ded1647ea4d48733b447
leningael <leningaelrs@gmail.com>
2024-05-03 16:39:58 -0600

    feat: :sparkles: Complaints service and abstractions for the repositories

---
+++ app/application/repositories/markers_repository.py
@@ -0,0 +1,15 @@
+
+from abc import ABC, abstractmethod
+from uuid import UUID
+
+from app.domain.models.location_model import LocationModel
+
+
+class MarkersRepository(ABC):
+    @abstractmethod
+    def add_marker(self, marker: LocationModel) -> LocationModel:
+        pass
+    
+    @abstractmethod
+    def get_marker_by_incident_id(self, incident_id: UUID) -> LocationModel:
+        pass
