403c8f578f75045fb19aa115d52590ecccce5494
leningael <leningaelrs@gmail.com>
2024-05-06 10:29:21 -0600

    feat: :sparkles: Filters added getting the complaints

--- app/application/repositories/complaints_repository.py
+++ app/application/repositories/complaints_repository.py
@@ -8,19 +8,24 @@ class ComplaintsRepository(ABC):
     @abstractmethod
     def add_complaint(self, complaint: ComplaintModel) -> ComplaintModel:
         pass
-    
+
     @abstractmethod
-    def get_complaints(self) -> list[ComplaintModel]:
+    def get_complaints(
+        self,
+        start_date: str | None = None,
+        end_date: str | None = None,
+        type_id: UUID | None = None,
+    ) -> list[ComplaintModel]:
         pass
 
     @abstractmethod
     def get_complaint(self, incident_id: UUID) -> ComplaintModel:
         pass
-    
+
     @abstractmethod
     def delete_complaint(self, incident_id: UUID) -> None:
         pass
-    
+
     @abstractmethod
     def update_complaint(self, complaint: ComplaintModel) -> ComplaintModel:
         pass
